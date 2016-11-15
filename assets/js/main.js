c07be32033bcd1567152cd392e887f905e18a10b
Yann Bertrand <yann@bertrand.com>
2016-11-15 21:43:30 +0100

    Create the user on form submission

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
+  var currentUser = {};
+
   $('form').on('submit', function () {
+    var userName = $('input#user-name').val();
+    io.socket.post('/user', { name: userName }, function (user, jwres) {
+      currentUser = user;
+    });
+
     return false;
   });
 
